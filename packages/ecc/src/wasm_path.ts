ce5c9b650fd7ffd8ea464ca40720fc7e04000a17
neeboo <ghostcorn@gmail.com>
2023-03-18 22:09:51 +0800

    :recycle: remove ecc package

--- packages/ecc/src/wasm_path.ts
+++
@@ -1,5 +0,0 @@
-import { URL, fileURLToPath } from 'url';
-export function path(wasmFilename) {
-  const url = new URL(wasmFilename);
-  return fileURLToPath(url);
-}
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,8 +1,6 @@
 cd packages
 cd adapter && rm -rf build && rm -rf *.tsbuildinfo && rm -rf package-lock.json
 cd ..
-cd ecc && rm -rf build && rm -rf dist && rm -rf *.tsbuildinfo && rm -rf package-lock.json
-cd ..
 cd snap && rm -rf build && rm -rf dist && rm -rf *.tsbuildinfo && rm -rf package-lock.json
 cd ..
 cd types && rm -rf build && rm -rf *.tsbuildinfo && rm -rf package-lock.json
